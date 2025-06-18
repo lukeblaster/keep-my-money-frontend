efe542fff2adde391c0c300dbed7413f074d1eb8
Lucas Soares <soaresl472@gmail.com>
2025-06-18 19:17:01 -0300

    chore: update axios instance

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL: "https://keep-my-money-backend.vercel.app",
   withCredentials: true,
-  timeout: 1000,
   headers: {
     "Content-Type": "application/json",
   },
