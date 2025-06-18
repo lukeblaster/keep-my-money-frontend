4971653f7b103a737e7843777605723b3dacd136
Lucas Soares <soaresl472@gmail.com>
2025-06-18 19:43:28 -0300

    .

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL: "https://keep-my-money-backend.vercel.app",
   withCredentials: true,
+  timeout: 6000,
   headers: {
     "Content-Type": "application/json",
   },
